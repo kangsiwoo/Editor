2a3ab492267ff11f77c657b374f8159e98d28ae9
kangsiwoo <2023003@bssm.hs.kr>
2024-10-05 08:26:30 +0900

    Initial Commit

---
+++ dataHandling/dataHandlingApp.swift
@@ -0,0 +1,32 @@
+//
+//  dataHandlingApp.swift
+//  dataHandling
+//
+//  Created by 강시우 on 10/5/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct dataHandlingApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ dataHandlingTests/dataHandlingTests.swift
@@ -0,0 +1,17 @@
+//
+//  dataHandlingTests.swift
+//  dataHandlingTests
+//
+//  Created by 강시우 on 10/5/24.
+//
+
+import Testing
+@testable import dataHandling
+
+struct dataHandlingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
